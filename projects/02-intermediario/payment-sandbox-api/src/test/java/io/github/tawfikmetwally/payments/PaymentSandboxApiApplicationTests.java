044137e3378abb6671473e18314adc696e575e33
Tawfik <tawfikaly02@gmail.com>
2026-08-29 07:38:59 -0300

    chore(payment-sandbox): configure persistence validation and health

--- projects/02-intermediario/payment-sandbox-api/src/test/java/io/github/tawfikmetwally/payments/PaymentSandboxApiApplicationTests.java
+++ projects/02-intermediario/payment-sandbox-api/src/test/java/io/github/tawfikmetwally/payments/PaymentSandboxApiApplicationTests.java
@@ -1,6 +1,11 @@
 package io.github.tawfikmetwally.payments;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.health.actuate.endpoint.HealthEndpoint;
+import org.springframework.boot.health.contributor.Status;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
@@ -8,8 +13,16 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class PaymentSandboxApiApplicationTests {
 
+	@Autowired
+	private HealthEndpoint healthEndpoint;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void applicationHealthIsUp() {
+		assertThat(healthEndpoint.health().getStatus()).isEqualTo(Status.UP);
 	}
 
 }
